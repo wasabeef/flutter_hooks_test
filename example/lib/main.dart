f922a0381003f3c83fed6a3e7a91c9b93a66615a
wasabeef <dadadada.chop@gmail.com>
2022-01-07 15:14:24 +0900

    chore: update sample code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,85 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:flutter_hooks_test/flutter_hooks_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'hooks/use_counter.dart';
+import 'hooks/use_latest.dart';
+import 'hooks/use_mount.dart';
+import 'hooks/use_update.dart';
+
+class MockEffect extends Mock {
+  VoidCallback? call();
+}
 
 void main() {
-  runApp(const MyApp());
-}
+  testWidgets('Not using flutter_hooks_test', (tester) async {
+    // Before
+    const key = Key('button');
+    var buildCount = 0;
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+    // called count is 1
+    await tester.pumpWidget(HookBuilder(builder: (context) {
+      final update = useUpdate();
+      buildCount++;
+      return GestureDetector(
+        key: key,
+        onTap: () => update(),
+      );
+    }));
+    // called count is 2
+    await tester.tap(find.byKey(key));
+    await tester.pumpAndSettle(const Duration(milliseconds: 1));
+    expect(buildCount, 2);
+  });
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  testWidgets('Using flutter_hooks_test', (tester) async {
+    // After
+    var buildCount = 0;
+    final result = await buildHook((_) {
+      buildCount++;
+      return useUpdate();
     });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    expect(buildCount, 1);
+    final update = result.current;
+    await act(() => update());
+    expect(buildCount, 2);
+  });
+
+  testWidgets('should rebuild after act()', (tester) async {
+    final result = await buildHook((_) => useCounter(5));
+    await act(() => result.current.inc());
+    expect(result.current.value, 6);
+  });
+
+  testWidgets('should unmount after unmount()', (tester) async {
+    final effect = MockEffect();
+    final result = await buildHook((_) => useMount(() => effect()));
+    verify(effect()).called(1);
+    verifyNoMoreInteractions(effect);
+    await result.unmount();
+    verifyNever(effect());
+    verifyNoMoreInteractions(effect);
+  });
+
+  testWidgets('should rebuild after rebuild()', (tester) async {
+    final effect = MockEffect();
+    final result = await buildHook((_) => useMount(() => effect()));
+    await result.rebuild();
+    verify(effect()).called(1);
+    verifyNoMoreInteractions(effect);
+  });
+
+  testWidgets('should rebuild after rebuild() with parameter', (tester) async {
+    final result = await buildHook(
+      (count) => useLatest(count),
+      initialProps: 123,
     );
-  }
+    expect(result.current, 123);
+    await result.rebuild(456);
+    expect(result.current, 456);
+  });
 }
